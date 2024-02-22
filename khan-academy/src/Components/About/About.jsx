355da81d17b9d346d5bbb3f1de5e11819f3442ed
Abhishak-Kumar <148067705+Abhishak-Kumar@users.noreply.github.com>
2024-02-22 22:43:54 +0530

    About

---
+++ khan-academy/src/Components/About/About.jsx
@@ -0,0 +1,158 @@
+// export const About = ()=>{
+//     return(
+//         <>
+//         <div>
+//         <div className="containerfull">
+//         <div className="about-page"><header className="title-section header-with-background-video"><hgroup className="title-container"><h1 className="title">Our mission is to provide a free, world‑className education for anyone, anywhere.</h1></hgroup><video className="responsive-video" loop="" autoplay=""><source src="https://cdn.kastatic.org/videos/homepage-background.mp4" type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;"><source src="https://cdn.kastatic.org/videos/homepage-background.webm" type="video/webm; codecs=&quot;vp8, vorbis&quot;"><source src="https://cdn.kastatic.org/videos/homepage-background.ogv" type="video/ogv; codecs=&quot;theora, vorbis&quot;"></video></header><nav className="about-nav" style="position: sticky; top: 0px;"><a href="/" className="about-nav-hamburger"><i className="icon-reorder"></i></a><ul className="about-nav-options about-nav-expanded"><li><a className="selected" href="/about">About</a></li><li><a href="https://blog.khanacademy.org/research/" target="_blank">Impact</a></li><li><a className="" href="/about/the-team">Team</a></li><li><a className="" href="/about/our-interns">Interns</a></li><li><a className="" href="/about/our-content-specialists">Content Specialists</a></li><li><a className="" href="/about/our-board">Leadership</a></li><li><a className="" href="/about/our-supporters">Supporters</a></li><li><a className="" href="/contribute">Translators</a></li><li><a className="" href="/about/blog">Blog</a></li></ul></nav>
+//                 <div className="about-shelf">
+//                     <div className="content">
+//                         <h2>A personalized learning resource for all ages</h2>
+//                         <p>Khan Academy offers practice exercises, instructional videos, and a personalized learning dashboard that empower learners to study at their own pace in and outside of the classNameroom. We tackle math, science, computing, history, art history, economics, and more, including K-14 and test preparation (SAT, Praxis, LSAT) content. We focus on skill mastery to help learners establish strong foundations, so there's no limit to what they can learn next!</p>
+//                         <div id="kui-button-1" className="center-button">
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className="about-shelf image-row teal">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/volume5.gif" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-07.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-12.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/volume5.gif" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-07.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-12.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/volume5.gif" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-07.jpg" alt="">
+//                 </div>
+//                 <div className="about-shelf">
+//                     <div className="content">
+//                         <h2>Free tools for parents and teachers</h2>
+//                         <p>We’re working hard to ensure that Khan Academy empowers coaches of all kinds to better understand what their children or students are up to and how best to help them. See at a glance whether a child or student is struggling or if she hit a streak and is now far ahead of the className. Our teacher dashboard provides a summary of className performance as a whole as well as detailed student profiles.</p>
+//                         <div id="coaching-button" className="center-button">
+//                             <a role="button" className="_194" href="/resources">
+//                                 <span className="_1gw4cnik">Learn more about coaching</span>
+//                             </a>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className="about-shelf image-row blue">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-09.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-13.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/graph.gif" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-10.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-09.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-13.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/graph.gif" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-10.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-09.jpg" alt="">
+//                     <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-13.jpg" alt="">
+//                 </div>
+//                 <div className="about-shelf">
+//                     <div className="content">
+//                         <h2>You’re joining a global classNameroom</h2>
+//                         <p>Millions of students from all over the world, each with their own unique story, learn at their own pace on Khan Academy every single day. Our resources are being translated into more than 36 languages in addition to the <a className="langs" href="https://es.khanacademy.org/">Spanish</a>, <a className="langs" href="https://fr.khanacademy.org/">French</a>, and <a className="langs" href="https://pt.khanacademy.org/">Brazilian Portuguese</a> versions of our site.</p>
+//                         <div id="stories-button" className="center-button">
+//                             <a role="button" className="_194" href="/stories">
+//                                 <span className="_1gw4cnik">Learn more about their stories</span>
+//                             </a>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className="about-shelf testimonials" style="background-color: rgb(84, 192, 166);">
+//                     <div className="content">
+//                         <!-- <button className="icon-button icon-button-left"></button>
+//                         <i className="icon-chevron-left navigation-arrow" aria-label="Previous testimonial"></i></button> -->
+//                         <ul aria-live="polite">
+//                             <li className="testimonial" style="background-color: rgb(84, 192, 166);">
+//                                 <blockquote>I just found your web site. I am 72 years old and I am now taking up learning where I left off. Thank you so much for all your hard work. </blockquote>
+//                                 <cite>Barbara</cite>
+//                             </li>
+//                         </ul>
+//                         <!-- <button className="icon-button icon-button-right">
+//                             <i className="icon-chevron-right navigation-arrow" aria-label="Next testimonial"></i>
+//                         </button> -->
+//                     </div>
+//                 </div>
+//                 <div className="about-shelf">
+//                     <div className="content">
+//                         <h2>From humble beginnings to a world-className team</h2>
+//                         <p>What started as one man tutoring his cousin has grown into a more than 150-person organization. We’re a diverse team that has come together to work on an audacious mission: to provide a free world-className education for anyone, anywhere. We are developers, teachers, designers, strategists, scientists, and content specialists who passionately believe in inspiring the world to learn. A few great people can make a big difference.</p>
+//                         <div id="team-button" className="center-button">
+//                             <a role="button" className="_194" href="/about/the-team">
+//                                 <span className="_1gw4cnik">Learn more about our team</span>
+//                             </a>
+//                         </div>
+//                     </div>
+//                 </div>
+//                     <div className="about-shelf donation-shelf" style="background-color: #52883e;">
+//                         <div className="content" style="color: white;">
+//                             <h2>Anyone can learn anything.  For free.</h2>
+//                             <p style="font-size: 18px;">Education is a human right.  We are a nonprofit because we believe in a free, world-className education for anyone, anywhere.  Instead of ads or subscriptions, we are supported by individual contributions from people like you.  Please join us today.</p>
+//                             <div id="donate-button" className="center-button">
+//                                 <a role="button" target="_blank" href="https://donate.khanacademy.org/give/419869/#!/donation/checkout?c_src2=Webapp.about" className="_z5xa1yk">
+//                                     <span className="_1gw4cnik">Donate</span>
+//                                 </a>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         </>
+//     )
+
+// }
+
+
+import React from 'react';
+
+export const About = () => {
+    return (
+        <>
+            <div>
+                <div className="containerfull">
+                    <div className="about-page">
+                        <header className="title-section header-with-background-video">
+                            <hgroup className="title-container">
+                                <h1 className="title">Our mission is to provide a free, world‑className education for anyone, anywhere.</h1>
+                            </hgroup>
+                            <video className="responsive-video" loop autoPlay>
+                                <source src="https://cdn.kastatic.org/videos/homepage-background.mp4" type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" />
+                                <source src="https://cdn.kastatic.org/videos/homepage-background.webm" type="video/webm; codecs=&quot;vp8, vorbis&quot;" />
+                                <source src="https://cdn.kastatic.org/videos/homepage-background.ogv" type="video/ogv; codecs=&quot;theora, vorbis&quot;" />
+                            </video>
+                        </header>
+                        <nav className="about-nav" style={{ position: "sticky", top: "0px" }}>
+                            <a href="/" className="about-nav-hamburger"><i className="icon-reorder"></i></a>
+                            <ul className="about-nav-options about-nav-expanded">
+                                <li><a className="selected" href="/about">About</a></li>
+                                <li><a href="https://blog.khanacademy.org/research/" target="_blank">Impact</a></li>
+                                <li><a className="" href="/about/the-team">Team</a></li>
+                                <li><a className="" href="/about/our-interns">Interns</a></li>
+                                <li><a className="" href="/about/our-content-specialists">Content Specialists</a></li>
+                                <li><a className="" href="/about/our-board">Leadership</a></li>
+                                <li><a className="" href="/about/our-supporters">Supporters</a></li>
+                                <li><a className="" href="/contribute">Translators</a></li>
+                                <li><a className="" href="/about/blog">Blog</a></li>
+                            </ul>
+                        </nav>
+                        <div className="about-shelf">
+                            <div className="content">
+                                <h2>A personalized learning resource for all ages</h2>
+                                <p>Khan Academy offers practice exercises, instructional videos, and a personalized learning dashboard that empower learners to study at their own pace in and outside of the classroom. We tackle math, science, computing, history, art history, economics, and more, including K-14 and test preparation (SAT, Praxis, LSAT) content. We focus on skill mastery to help learners establish strong foundations, so there's no limit to what they can learn next!</p>
+                                <div id="kui-button-1" className="center-button"></div>
+                            </div>
+                        </div>
+                        <div className="about-shelf image-row teal">
+                            <img src="https://cdn.kastatic.org/images/about/product_images/volume5.gif" alt="" />
+                            <img src="https://cdn.kastatic.org/images/about/product_images/about_withgifs-07.jpg" alt="" />
+                            {/* Include other images here */}
+                        </div>
+                        {/* Include other shelves here */}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default About;
+
